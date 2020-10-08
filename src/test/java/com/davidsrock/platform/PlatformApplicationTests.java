c856c927e5d5f595c5c2acd7fcd13df8aa44b431
Kithmal <kithmal91@gmail.com>
2020-10-08 23:41:54 +0530

    added files

---
+++ src/test/java/com/davidsrock/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidsrock.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
